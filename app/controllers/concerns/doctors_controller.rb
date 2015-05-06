47695baa626939ddffe1243fbe190b8ead58d65f
snoopyrichie <rmtsai03@gmail.com>
2015-05-06 10:01:52 -0500

    KIEI-925 Homework #5
    KIEI-925 Homework #5

--- app/controllers/concerns/doctors_controller.rb
+++ app/controllers/concerns/doctors_controller.rb
@@ -1,28 +1,25 @@
 class DoctorsController < ApplicationController
 
   def index
-  @doctor = Doctors.all
+    @doctors = Doctors.all
   end
 
   def new
-  @doctor = Doctors.new
+    @doctors = Doctors.new
   end
 
-  def show 
-  end
-  	
   def create
   	Doctors.create(params["doctors"])
    	redirect_to doctors_url
   end
 
   def edit
-  	@doctor = Doctors.find_by(id: params["id"])
+  	@doctors = Doctors.find_by(id: params["id"])
   end
 
   def update
-  	@doctor = Doctors.find_by(id: params["id"])
-  	@doctor.update(params["doctors"])
+  	@doctors = Doctors.find_by(id: params["id"])
+  	@doctors.update(params["doctors"])
   	redirect_to doctors_url
   end
 
@@ -31,10 +28,4 @@ class DoctorsController < ApplicationController
   	@doctor.delete
 	redirect_to doctors_url
   end	
-
 end
-
-
-
-
-  
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get '/doctors', to: 'doctors#index'
-  get '/doctors/new', to: 'doctors#new'
-  post '/doctors', to: 'doctors#create'
-  get '/doctors/:id', to: 'doctors#show'
-  get '/doctors/:id/edit', to: 'doctors#edit'
-  patch '/doctors/:id', to: 'doctors#update'
-  delete '/doctors/:id', to: 'doctors#destroy'
+	resources :doctors
 end
