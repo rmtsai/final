404242e50e5788b649673b3c2ff9660f8ee6c3b5
snoopyrichie <rmtsai03@gmail.com>
2015-06-02 13:23:23 -0500

    KIEI-925 Final Update
    KIEI-925 Final Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,25 @@
 class UsersController < ApplicationController
-
+skip_before_action :auth
   def new
   @user = User.new
   end
   	
   def create
   	
-     @user = User.create(params["user"])
+     @user = User.create(name: params["user"]) 
     if @user.valid?
       @doctor=Doctor.create
       @user.doctor_id=@doctor.id
       @doctor.name = @user.name 
       @doctor.save
-      redirect_to doctors_path
+      redirect_to home_index_path
     else
       render "new"
     end
   end
 
 end
+
 
 
 
